2c4bc5392d1ca2527c3bfa6a6c645bf43b6a5c7b
Daniel <danielmtdc16@gmail.com>
2023-06-03 15:54:42 -0300

    alteração no style.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let url_qrCode = document.querySelector("#url-qrcode");
+let warning = document.querySelector("#warning");
 let btn_generate = document.querySelector("#btn-generate");
 let qr_code = document.querySelector("#qr-code")
 
+btn_generate.addEventListener('click', () => {
+
+    if (url_qrCode.value) {
+
+    } else {
+        warning.innerHTML = "coloque uma URL para gerar!";
+    }
+
+});
